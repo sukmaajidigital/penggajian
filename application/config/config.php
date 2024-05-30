f4bec9d90dc0c116d60bdf2cfc35bcf20920f02a
sukmaajidigital <114824269+sukmaajidigital@users.noreply.github.com>
2024-05-31 05:48:59 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost/penggajian/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$config['index_page'] = 'Landing_Page.php';
 
 /*
 |--------------------------------------------------------------------------
